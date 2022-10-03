028b4cf3b4ec3076bc17514d8df8c4943d4a9380
Duda Araujo <mduda_araujo@hotmail.com>
2022-10-03 19:25:20 -0300

    Populando tabelas

---
+++ Alura.ByteBank/Alura.ByteBank.Infraestrutura.Testes/ByteBankContextoTestes.cs
@@ -0,0 +1,32 @@
+﻿using Alura.ByteBank.Dados.Contexto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.ByteBank.Infraestrutura.Testes
+{
+    public class ByteBankContextoTestes
+    {
+        [Fact]
+        public void TestaConexaoContextoComMySQL()
+        {
+            var contexto = new ByteBankContexto();
+            bool conectado;
+
+            try
+            {
+                conectado = contexto.Database.CanConnect();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Não foi possível conectar a base de dados");
+
+            }
+
+            Assert.True(conectado);
+        }
+    }
+}
---
+++ Alura.ByteBank/Alura.ByteBank.Infraestrutura.Testes/ClienteRepositorioTestes.cs
@@ -0,0 +1,39 @@
+﻿using Alura.ByteBank.Dados.Repositorio;
+using Alura.ByteBank.Dominio.Entidades;
+using Alura.ByteBank.Dominio.Interfaces.Repositorios;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.ByteBank.Infraestrutura.Testes
+{
+    public class ClienteRepositorioTestes
+    {
+        private readonly IClienteRepositorio _repositorio;
+        public ClienteRepositorioTestes()
+        {
+            var servico = new ServiceCollection();
+            servico.AddTransient<IClienteRepositorio, ClienteRepositorio>();
+            var provedor = servico.BuildServiceProvider();
+            _repositorio = provedor.GetService<IClienteRepositorio>();
+        }
+
+        [Fact]
+        public void TestaObterTodosClientes()
+        {
+            //Arrange
+        
+
+            //Act
+            List<Cliente> lista = _repositorio.ObterTodos();
+
+            //Assert
+            Assert.NotNull(lista);
+            Assert.Equal(4, lista.Count);
+        }
+    }
+}
